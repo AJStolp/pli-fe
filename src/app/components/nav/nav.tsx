edb8323ffd0839dcf24a3545543cb0f0142a5f6a
Anthony Stolp <anthony.stolp@gmail.com>
2023-12-28 11:41:35 -0500

    nav interface

--- src/app/components/nav/nav.tsx
+++ src/app/components/nav/nav.tsx
@@ -6,35 +6,41 @@ import {
   NavbarLink,
   NavbarToggle,
 } from "flowbite-react";
+import { NavData } from "../../interfaces/returned-data/navigation";
+import { getData } from "../../api/fetch";
+import { Suspense } from "react";
 
-export default function Navigation() {
-  return (
-    <Navbar fluid rounded className="bg-inherit p-6">
-      <NavbarBrand as={Link} href="https://flowbite-react.com">
-        <img
-          src="https://cdn.pixabay.com/photo/2021/10/17/13/41/polar-bear-6718362_1280.jpg"
-          className="mr-3 h-6 sm:h-9"
-          alt="PLI"
-        />
-      </NavbarBrand>
-      <NavbarToggle />
-      <NavbarCollapse>
-        <NavbarLink href="#" active className="text-xl">
-          Home
-        </NavbarLink>
-        <NavbarLink as={Link} href="#" className="text-xl">
-          About
-        </NavbarLink>
-        <NavbarLink href="#" className="text-xl">
-          Services
-        </NavbarLink>
-        <NavbarLink href="#" className="text-xl">
-          Map
-        </NavbarLink>
-        <NavbarLink href="#" className="text-xl">
-          Contact
-        </NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
-  );
+export default async function Navigation() {
+  const endpoint = "/api/navigations?populate=*";
+
+  try {
+    const data: NavData = await getData(endpoint);
+    console.log(data.id, "Dataaa");
+
+    return (
+      <Suspense fallback={"Loading"}>
+        <Navbar fluid rounded className="bg-inherit p-6">
+          <NavbarBrand as={Link} href="https://flowbite-react.com">
+            <img
+              src="https://cdn.pixabay.com/photo/2021/10/17/13/41/polar-bear-6718362_1280.jpg"
+              className="mr-3 h-6 sm:h-9"
+              alt="PLI"
+            />
+          </NavbarBrand>
+          <NavbarToggle />
+          <NavbarCollapse>
+            {data.attributes.linktext.map((val) => {
+              return (
+                <NavbarLink href="#" active className="text-xl">
+                  {val.linktext}
+                </NavbarLink>
+              );
+            })}
+          </NavbarCollapse>
+        </Navbar>
+      </Suspense>
+    );
+  } catch (error) {
+    return <div>Error:</div>;
+  }
 }
