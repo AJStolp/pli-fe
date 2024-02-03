eea9f0c4ad8f4bafd7d34781db7b2db348658fca
Anthony Stolp <anthony.stolp@gmail.com>
2024-02-03 00:00:03 -0500

    try this

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,7 +5,7 @@ import { Footer, FooterCopyright, FooterIcon } from "flowbite-react";
 export default async function FooterComponent() {
   const currentYear = new Date().getFullYear();
 
-  const endpoint = `${process.env.NEXT_PUBLIC_STRAPI_BE_URL}/api/socialmedias?populate[iconsandlinks][populate][icon][populate]`;
+  const endpoint = `https://pli-be-production.up.railway.app/api/socialmedias?populate[iconsandlinks][populate][icon][populate]`;
 
   try {
     const data: SocialMedia[] = await getData(endpoint);
